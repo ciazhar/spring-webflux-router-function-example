56693741f10f916296a0703352b2a40e154d913b
ciazhar <ciazhar.id@gmail.com>
2018-01-04 06:24:38 +0700

    update repository

--- src/main/java/com/ciazhar/authservice/repository/UserRepository.java
+++ src/main/java/com/ciazhar/authservice/repository/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import com.ciazhar.authservice.model.User;
 
+import reactor.core.publisher.Mono;
+
 public interface UserRepository extends ReactiveMongoRepository<User,String>{
-    
+    Mono<User> findByUsername(String username);
 }
